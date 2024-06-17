687173f9fbf3936de01246fd5ae3dd110f9a54f1
svetlana alaberdina <salaberdina@mail.ru>
2024-06-17 23:50:57 +0300

    learn tests

--- main.go
+++ main.go
@@ -2,10 +2,38 @@ package main
 
 import "fmt"
 
+const (
+	englishHelloPrefix = "hello, "
+	spanishHelloPrefix = "hola, "
+	frenchHelloPrefix  = "bonjour, "
+	havaiHelloPrefix   = "aloha, "
+
+	spanish = "spanish"
+	french  = "french"
+	havai   = "havai"
+)
+
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("", ""))
 }
 
-func Hello() string {
-	return "hello world!"
+func Hello(name, language string) string {
+	if name == "" {
+		name = "world"
+	}
+
+	return getGreetingPrefix(language) + name
+}
+
+func getGreetingPrefix(language string) string {
+	switch language {
+	case french:
+		return frenchHelloPrefix
+	case spanish:
+		return spanishHelloPrefix
+	case havai:
+		return havaiHelloPrefix
+	default:
+		return englishHelloPrefix
+	}
 }
